b9136ab014eaf5f95d46ac813d9e17fa953698a5
justin_wan <justin_wan@hansight.com>
2016-08-13 15:24:12 +0800

    Add more nuanced color palette

--- src/kibana/components/vislib/components/color/seed_colors.js
+++ src/kibana/components/vislib/components/color/seed_colors.js
@@ -4,17 +4,62 @@ define(function () {
    * So we needed to come up with a color scheme of our own that creates consistent, pleasing color patterns.
    * The order allows us to guarantee that 1st, 2nd, 3rd, etc values always get the same color.
    * Returns an array of 72 colors.
+   *
+   * 使用 http://www.herethere.net/~samson/php/color_gradient/ 产生渐变色。
+   * 集中在人眼最敏感的蓝绿色系（http://cms.nyipcn.com/uploadfile/2014/0727/20140727113805928.jpg）
+   *
    */
+
 
   return function SeedColorUtilService() {
     return [
-      '#57c17b',
-      '#006e8a',
-      '#6f87d8',
-      '#663db8',
-      '#bc52bc',
-      '#9e3533',
-      '#daa05d'
+      '#4377AA',
+      '#4884AC',
+      '#4E92AE',
+      '#539FB0',
+      '#59ADB2',
+
+      '#5FBBB4',
+      '#56B79E',
+      '#4DB388',
+      '#45AF73',
+      '#3CAB5D',
+
+      '#34A848',
+      '#5AAF40',
+      '#80B639',
+      '#A6BD32',
+      '#CCC42B',
+
+      '#F3CB24',
+      '#EAB323',
+      '#E19B22',
+
+      '#D98321',
+      '#E58416',
+      '#F2850B',
+
+      '#FF8700',
+      '#E0801E',
+      '#C1793D',
+
+      '#A2735C',
+      '#BD6158',
+      '#D84F54',
+
+      '#F43D50',
+      '#F7596C',
+      '#FB7588',
+
+      '#FF92A4',
+      '#E887A3',
+      '#D17CA2',
+
+      '#BA72A2',
+      '#9273A4',
+      '#6A75A7'
+
+
     ];
   };
 });
